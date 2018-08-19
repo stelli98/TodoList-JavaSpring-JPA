b4c79b1075eb72745141fc278e3864adf27b2ba4
stelli98 <stellitan98@gmail.com>
2018-08-19 14:39:00 +0700

    V.1.0.0

---
+++ todolist/src/main/java/com/example/postgresdemo/model/Todo.java
@@ -0,0 +1,53 @@
+package com.example.postgresdemo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Todo")
+public class Todo extends AuditModel {
+    @Id
+    @GeneratedValue(generator = "todo_generator")
+    @SequenceGenerator(
+            name = "todo_generator",
+            sequenceName = "todo_sequence",
+            initialValue = 1
+    )
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getUser(){
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user= user;
+    }
+}
---
+++ todolist/src/main/java/com/example/postgresdemo/model/User.java
@@ -0,0 +1,41 @@
+package com.example.postgresdemo.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User extends AuditModel {
+    @Id
+    @GeneratedValue(generator = "user_generator")
+    @SequenceGenerator(
+            name = "user_generator",
+            sequenceName = "user_sequence",
+            initialValue = 1
+    )
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String name;
+
+    @Column(columnDefinition = "text")
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
